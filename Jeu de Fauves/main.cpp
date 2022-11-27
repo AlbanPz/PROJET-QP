23a6377a908339497cc244630c440af93cb37d75
Mamadou Dian DIALLo <mdiandalein@gmail.com>
2022-11-27 19:29:28 +0100

    Création du projet principal et du projet de tests unitaires

---
+++ Jeu de Fauves/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+    Ceci est le projet principal pour le Jeu
+*/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    /*
+
+        Nous avons là deux projet bien distinct les fonctions et méthodes définit
+        dans l'un n'est pas visible dans l'autre
+
+    */
+    return 0;
+}
---
+++ Test Jeu de Fauves/testmain.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+    Ceci est le projet de tests unitaires pour le Jeu
+*/
+
+void test() // Invisible dans l'autre projet
+{
+    cout << "Salut je suis la fn de test !" << endl;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    test();
+    return 0;
+}
