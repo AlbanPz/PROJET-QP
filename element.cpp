b95e9b40fe3929a305ecc216ec5875fcd9c81d1f
Mamadou Dian DIALLo <mdiandalein@gmail.com>
2022-11-28 19:41:07 +0100

    Ajout de la classe Element avec la définition de ses méthodes

--- element.cpp
+++ element.cpp
@@ -1,3 +1,15 @@
 #include "element.h"
 
+Element::Element(int x, int y): d_x{x}, d_y{y}
+{}
+
+int Element::x() const
+{
+    return d_x;
+}
+
+int Element::y() const
+{
+    return d_y;
+}
 
--- element.h
+++ element.h
@@ -1,6 +1,17 @@
 #ifndef ELEMENT_H_INCLUDED
 #define ELEMENT_H_INCLUDED
 
-
+class Element
+{
+public:
+    Element(int x, int y);
+    int x() const;
+    int y() const;
+    virtual char affiche() const =0;
+    virtual ~Element() =default;
+private:
+    int d_x;
+    int d_y;
+};
 
 #endif // ELEMENT_H_INCLUDED
