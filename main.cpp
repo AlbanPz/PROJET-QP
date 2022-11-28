df15c0ef76cacc146b4a8ba46e1943c6d9963b7e
Mamadou Dian DIALLo <mdiandalein@gmail.com>
2022-11-28 16:11:23 +0100

    Mise à jour du projet

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+    Ceci est le projet principal pour le Jeu
+*/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    /**
+        Nous avons là deux projets bien distincts
+    */
+    return 0;
+}
