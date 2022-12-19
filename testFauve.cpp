7b425d5fa70fc6d9350e66a4b3d0ae96d71ef364
Alban <101833237+AlbanPz@users.noreply.github.com>
2022-12-19 21:33:48 +0100

    Ajout de 3 tets
    "Changement de position de Fauve" : vérifie que la class Fauve modifie correctement les coordonnées de l'objet.
    "Fauve avec déplacement diagonal" : vérifie que la class Fauve peut se déplacer sur des cases diagonales si son type de déplacement le permet.
    "Fauve avec force négative" : vérifie que la force d'un fauve ne peut pas être négative.

--- testFauve.cpp
+++ testFauve.cpp
@@ -19,3 +19,22 @@ TEST_CASE("Fauve peut se déplacer sur position") {
   CHECK(fauve.peutSeDeplacerSurPosition(1, 0));  // Déplacement horizontal
   CHECK(!fauve.peutSeDeplacerSurPosition(1, 1));  // Déplacement diagonal
 }
+
+TEST_CASE("Changement de position de Fauve") {
+Fauve fauve("Lion", 0, 0, 10, Mobile::TypeDeplacement::VERTI_HORI);
+fauve.changerPosition(1, 1);
+CHECK(fauve.x() == 1);
+CHECK(fauve.y() == 1);
+}
+
+TEST_CASE("Fauve avec déplacement diagonal") {
+Fauve fauve("Tigre", 0, 0, 10, Mobile::TypeDeplacement::DIAGONALE);
+CHECK(fauve.peutSeDeplacerSurPosition(0, 1)); // Déplacement vertical
+CHECK(fauve.peutSeDeplacerSurPosition(1, 0)); // Déplacement horizontal
+CHECK(fauve.peutSeDeplacerSurPosition(1, 1)); // Déplacement diagonal
+}
+
+TEST_CASE("Fauve avec force négative") {
+Fauve fauve("Lion", 0, 0, -10, Mobile::TypeDeplacement::VERTI_HORI);
+CHECK(fauve.force() == 0); // La force ne peut pas être négative
+}
