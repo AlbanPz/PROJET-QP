fe5080cbae777bc6f1446bb4b5be6d8a699303c3
Alban <101833237+AlbanPz@users.noreply.github.com>
2022-12-19 21:27:41 +0100

    Ajout de deux cas de test
    "Constructeur de Fauve" : vérifie que le constructeur de la class Fauve initialise correctement les différentes valeurs de l'objet.
    "Fauve peut se déplacer sur position" : vérifie que la méthode peutSeDeplacerSurPosition de la class Fauve renvoie les résultats attendus en fonction du type de déplacement du fauve.

--- testFauve.cpp
+++ testFauve.cpp
@@ -4,3 +4,18 @@
 
 using std::string;
 
+TEST_CASE("Constructeur de Fauve") {
+  Fauve fauve("Lion", 0, 0, 10, Mobile::TypeDeplacement::VERTI_HORI);
+  CHECK(fauve.nom() == "Lion");
+  CHECK(fauve.x() == 0);
+  CHECK(fauve.y() == 0);
+  CHECK(fauve.force() == 10);
+  CHECK(fauve.typeDeplacement() == Mobile::TypeDeplacement::VERTI_HORI);
+}
+
+TEST_CASE("Fauve peut se déplacer sur position") {
+  Fauve fauve("Lion", 0, 0, 10, Mobile::TypeDeplacement::VERTI_HORI);
+  CHECK(fauve.peutSeDeplacerSurPosition(0, 1));  // Déplacement vertical
+  CHECK(fauve.peutSeDeplacerSurPosition(1, 0));  // Déplacement horizontal
+  CHECK(!fauve.peutSeDeplacerSurPosition(1, 1));  // Déplacement diagonal
+}
