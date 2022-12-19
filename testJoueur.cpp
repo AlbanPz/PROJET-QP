80509ce01e42638f40252d0c39c98e7a925cde7d
Alban <101833237+AlbanPz@users.noreply.github.com>
2022-12-19 22:13:22 +0100

    Update testJoueur.cpp

--- testJoueur.cpp
+++ testJoueur.cpp
@@ -7,20 +7,20 @@ using std::string;
 TEST_CASE("Test du constructeur de Joueur") {
     // Test avec des valeurs de base valides
     Joueur joueur("Joueur 1", 0, 0, 10, 1);
-    CHECK(joueur.nom() == "Joueur 1");
-    CHECK(joueur.x() == 0);
-    CHECK(joueur.y() == 0);
-    CHECK(joueur.force() == 10);
-    CHECK(joueur.typeDeplacement() == 1);
+    REQUIRE_EQ(joueur.nom(), "Joueur 1");
+    REQUIRE_EQ(joueur.x(), 0);
+    REQUIRE_EQ(joueur.y(), 0);
+    REQUIRE_EQ(joueur.force(), 10);
+    REQUIRE_EQ(joueur.typeDeplacement(), 1);
 
     // Test avec des valeurs limites valides
     Joueur joueur2("Joueur 2", 0, 0, std::numeric_limits<int>::max(), 1); 
     // std::numeric_limits<int>::max() : la valeur maximale que peut prendre un type entier
-    CHECK(joueur2.nom() == "Joueur 2");
-    CHECK(joueur2.x() == 0);
-    CHECK(joueur2.y() == 0);
-    CHECK(joueur2.force() == std::numeric_limits<int>::max());
-    CHECK(joueur2.typeDeplacement() == 1);
+    REQUIRE_EQ(joueur2.nom(), "Joueur 2");
+    REQUIRE_EQ(joueur2.x(), 0);
+    REQUIRE_EQ(joueur2.y(), 0);
+    REQUIRE_EQ(joueur2.force(), std::numeric_limits<int>::max());
+    REQUIRE_EQ(joueur2.typeDeplacement(), 1);
 }
 
 TEST_CASE("Test de la méthode peutSeDeplacerSurPosition de Joueur") {
