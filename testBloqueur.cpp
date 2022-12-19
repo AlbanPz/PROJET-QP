ee90b5b13127cb68802edadd740b92566cb95296
Alban <101833237+AlbanPz@users.noreply.github.com>
2022-12-19 22:10:07 +0100

    Update testBloqueur.cpp

--- testBloqueur.cpp
+++ testBloqueur.cpp
@@ -7,12 +7,12 @@ using std::string;
 TEST_CASE("Test de la classe Bloqueur")
 {
     Bloqueur b("Arbre", 3, 4);
-    CHECK(b.nom() == "Arbre");
-    CHECK(b.x() == 3);
-    CHECK(b.y() == 4);
-    CHECK(b.bloqueLePassage() == true);
+    REQUIRE_EQ(b.nom(), "Arbre");
+    REQUIRE_EQ(b.x(), 3);
+    REQUIRE_EQ(b.y(), 4);
+    REQUIRE_EQ(b.bloqueLePassage(), true);
 
     b.changerPosition(5, 6);
-    CHECK(b.x() == 5);
-    CHECK(b.y() == 6);
+    REQUIRE_EQ(b.x(), 5);
+    REQUIRE_EQ(b.y(), 6);
 }
