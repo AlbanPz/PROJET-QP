eed7621bbe267978e1682e4671549851a6f0145c
Alban <101833237+AlbanPz@users.noreply.github.com>
2022-12-19 22:04:09 +0100

    Ajout d'un test
    "Test de la classe Bloqueur" : vérifie si un obstacle peut bloquez le passage des fauves.

--- testBloqueur.cpp
+++ testBloqueur.cpp
@@ -4,3 +4,15 @@
 
 using std::string;
 
+TEST_CASE("Test de la classe Bloqueur")
+{
+    Bloqueur b("Arbre", 3, 4);
+    CHECK(b.nom() == "Arbre");
+    CHECK(b.x() == 3);
+    CHECK(b.y() == 4);
+    CHECK(b.bloqueLePassage() == true);
+
+    b.changerPosition(5, 6);
+    CHECK(b.x() == 5);
+    CHECK(b.y() == 6);
+}
