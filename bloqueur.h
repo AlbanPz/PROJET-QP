42c48cd8db6676ea4fab0b88f1950af90573081f
Alban <101833237+AlbanPz@users.noreply.github.com>
2022-12-19 22:05:32 +0100

    Ajout de la méthode bloqueLePassage()

--- bloqueur.h
+++ bloqueur.h
@@ -4,4 +4,5 @@
 class Bloqueur : public Element {
 public:
     Bloqueur(const std::string& nom, int x, int y);
+    bool bloqueLePassage() const { return true; } // renvoie toujours true car elle indique que l'obstacke bloque le passage
 };
