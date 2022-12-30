f15c22559c2e1727e96edafea164317b8abbc8f4
Cedric1425 <118215545+Cedric1425@users.noreply.github.com>
2022-12-30 11:56:41 +0100

    vdfb

--- testMobile.cpp
+++ testMobile.cpp
@@ -1,43 +1,18 @@
 #include "doctest.h"
 #include "mobile.h"
 
-/*
-
-TEST_CASE("[Point] Les points sont bien construits") {
-//constructeur avec x et y
-double x {31.21}, y {-32.3};
-point p1 {x,y};
-REQUIRE( p1.x() == x );
-REQUIRE( p1.y() == y );
-//constructeur par défaut
-point p2 {};
-REQUIRE( p2.x() == 0.0 );
-REQUIRE( p2.y() == 0.0 );
-}
-<<<<<<< HEAD
-
-*/
-
-/** Cette partie ne compilait pas Cedric */
-
-/*
-=======
-*/
 
 
-/** Cette partie ne compilait pas Cedric */
 
 
->>>>>>> 9238379a0c20649091504fe304769ee693374832
+
 TEST_CASE("[Mobile] Le mobile est bien construit") {
  // constructeur avec x, y, force et typeDeplacement
  int x = 10, y = 15, force = 30, typeDeplacement = 10;
 
-<<<<<<< HEAD
-    Mobile a {x,y,force,typeDeplacement};
-=======
+
     Mobile a {"test",x,y,force,typeDeplacement};
->>>>>>> 9238379a0c20649091504fe304769ee693374832
+
 
     REQUIRE( a.x() == x );
     REQUIRE( a.y() == y );
@@ -45,8 +20,8 @@ TEST_CASE("[Mobile] Le mobile est bien construit") {
     REQUIRE( a.typeDeplacement() == typeDeplacement );
 
 }
-<<<<<<< HEAD
-*/
-=======
 
->>>>>>> 9238379a0c20649091504fe304769ee693374832
+
+
+
+
