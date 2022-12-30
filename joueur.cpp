0f4886d7f5b7f9c61edc56a2c87a0cd986393273
JalluhLps <mdiandalein@gmail.com>
2022-12-30 22:30:55 +0100

    Décla et déf de la méthode virt. seDeplacer() + ajout des méthodes kill() et estVivant() + données d_vie

--- joueur.cpp
+++ joueur.cpp
@@ -5,7 +5,26 @@
 * Constructeur de joueru
 
 Joueur::Joueur(const std::string& nom, int x, int y, int force, int typeDeplacement) :
-    Mobile{nom, x, y, force, typeDeplacement}
+    Mobile{nom, x, y, force, typeDeplacement}, d_vie{true}
 {}
 */
+
+/** Rajouté par Mdou Dian */
+
+bool Joueur::seDeplacer(Mobile* mobile, int x, int y)
+{
+    kill();
+    mobile->changerPosition(x, y);
+    return estVivant();
+}
+
+bool Joueur::estVivant() const
+{
+    return d_vie;
+}
+
+void Joueur::kill()
+{
+    d_vie = false;
+}
 
--- joueur.h
+++ joueur.h
@@ -1,7 +1,15 @@
 #include "mobile.h"
 
 
-class Joueur : public Mobile{
+class Joueur : public Mobile
+{
 public:
     Joueur(const std::string& nom, int x, int y, int force, int typeDeplacement);
+    bool seDeplacer(Mobile* mobile, int x, int y) override;
+
+/** Rajouté par Mdou Dian */
+    bool estVivant() const;
+    void kill();
+private:
+    bool d_vie;
 };
