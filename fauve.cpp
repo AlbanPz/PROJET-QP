939458ca7d2f111309c2d471fb55773d6fb13e2a
JalluhLps <mdiandalein@gmail.com>
2022-12-30 22:35:04 +0100

    Déclaration et définition de la méthode virtuelle seDeplacer()

--- fauve.cpp
+++ fauve.cpp
@@ -7,3 +7,15 @@ Fauve::Fauve(const std::string& nom, int x, int y, int force, int typeDeplacemen
     Mobile{nom, x, y, force, typeDeplacement}
 {}
 */
+
+/** Rajouté par Mdou Dian */
+bool Fauve::seDeplacer(Mobile* mobile, int x, int y)
+{
+    if (mobile->force() > force())
+    {
+        mobile->changerPosition(x, y);
+        return true;
+    }else
+        return false;
+}
+
--- fauve.h
+++ fauve.h
@@ -4,4 +4,7 @@
 class Fauve : public Mobile{
 public:
     Fauve(const std::string& nom, int x, int y, int force, int typeDeplacement);
+
+    /** Rajouté par Mdou Dian */
+    bool seDeplacer(Mobile* mobile, int x, int y) override;
 };
