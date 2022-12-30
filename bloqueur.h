ee69f6b5e532064e7a09e994088936d904914696
JalluhLps <mdiandalein@gmail.com>
2022-12-30 22:37:49 +0100

    Déclaration de la méthode virtuelle seDeplacer() en override = 0

--- bloqueur.h
+++ bloqueur.h
@@ -5,4 +5,7 @@ class Bloqueur : public Element {
 public:
     Bloqueur(const std::string& nom, int x, int y);
     bool bloqueLePassage() const { return true; } // renvoie toujours true car elle indique que l'obstacke bloque le passage
+
+    /** Rajouté par Mdou Dian */
+    bool seDeplacer(Mobile* mobile, int x, int y) override = 0;
 };
