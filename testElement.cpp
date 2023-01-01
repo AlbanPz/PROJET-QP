342fb8e3733737708f9f777c2d83f084c91a0bd0
JalluhLps <mdiandalein@gmail.com>
2023-01-01 13:58:21 +0100

    MAJ des tests de la classe Element

--- testElement.cpp
+++ testElement.cpp
@@ -1,16 +1,36 @@
 #include "doctest.h"
 #include "element.h"
+#include "fauve.h"
 #include <string>
 
 using std::string;
 
 TEST_CASE("[Element] L'element est bien construit")
 {
-    string nom{"Lyon"};
+    string nom{"Lion"};
     int x = 0, y = 0;
-    Element e{nom, x, y};
+    Fauve f{nom, x, y, 10, 2};
 
-    REQUIRE_EQ(nom, e.nom());
-    REQUIRE_EQ(x, e.x());
-    REQUIRE_EQ(y, e.y());
+    SUBCASE("Le nom de l'element est correct")
+    {
+        REQUIRE_EQ(f.nom(), nom);
+    }
+
+    SUBCASE("La position de l'element est juste")
+    {
+        REQUIRE_EQ(f.x(), x);
+        REQUIRE_EQ(f.y(), y);
+    }
 }
+
+TEST_CASE("[Element] Le changement de position est correct")
+{
+    string nom{"Lion"};
+    int x = 0, y = 0;
+    Fauve f{nom, x, y, 10, 2};
+    f.changerPosition(x+1, y-1);
+
+    REQUIRE_EQ(f.x(), x+1);
+    REQUIRE_EQ(f.y(), y-1);
+}
+
