8f97fb4f2eaa740a9db8f80a8f89f5afa8a8c9fe
Samib68 <118477587+Samib68@users.noreply.github.com>
2023-01-04 19:21:52 +0100

    Update afficheurCarteConsole.cpp

--- afficheurCarteConsole.cpp
+++ afficheurCarteConsole.cpp
@@ -8,11 +8,11 @@ void afficheurCarteConsole::afficherCarte(const Carte &c) const
         for(int j=0;j<c.d_grille[i].size;j++)
         {
           if(d_grille[i][j]==nullptr)
-                cout<<".";
+                std::cout<<".";
           else
-            std::out<<d_grille[i][j]->d_nom[0];
+            std::cout<<d_grille[i][j]->d_nom[0];
         }
-        std::out<<endl;
+        std::cout<<endl;
     }
 
 }
