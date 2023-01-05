45cb4d723d8e2a3ab2e3b5c7c1fec958cf01a0fb
Samib68 <118477587+Samib68@users.noreply.github.com>
2023-01-05 14:28:09 +0100

    Add files via upload

--- afficheurCarteConsole.cpp
+++ afficheurCarteConsole.cpp
@@ -3,14 +3,14 @@
 #include <vector>
 void afficheurCarteConsole::afficherCarte(const Carte &c) const
 {
-    for(int i=0; i<c.d_grille.size;i++)
+    for(int i=0; i<c.ligne();i++)
     {
-        for(int j=0;j<c.d_grille[i].size;j++)
+        for(int j=0;j<c.colonne();j++)
         {
-          if(d_grille[i][j]==nullptr)
+          if(c.elementALaPosition(i,j)==nullptr)
                 std::cout<<".";
           else
-            std::cout<<d_grille[i][j]->d_nom[0];
+            std::cout<<c.elementALaPosition(i,j)->d_nom[0];
         }
         std::cout<<endl;
     }
