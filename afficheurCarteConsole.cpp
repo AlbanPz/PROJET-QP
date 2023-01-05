4dd0ddd3bfb320e0691bb424b17f530a2e53138f
Cedric1425 <118215545+Cedric1425@users.noreply.github.com>
2023-01-05 17:10:00 +0100

    Amélioration de la mise en forme

--- afficheurCarteConsole.cpp
+++ afficheurCarteConsole.cpp
@@ -1,22 +1,40 @@
+
 #include "carte.h"
 #include "afficheurCarteConsole.h"
 #include <vector>
-void AfficheurCarteConsole::AfficherCarte(const Carte &c) const
+
+void AfficheurCarteConsole::AfficherCarte( const Carte &c) const
 {
+    // ligne du haut
+    std::cout<<' ';
+    for(int i=0; i< 3*c.nombreLignes();i++) std::cout<<'_';
+    std::cout<<std::endl;
+
+
     for(int i=0; i<c.nombreLignes();i++)
     {
+        //ligne de gauche
+        std::cout<<'|';
         for(int j=0;j<c.nombreColonnes();j++)
         {
           if( c.elementALaPosition(i,j) ==nullptr)
-                std::cout<<".";
+                std::cout<< std::setw(3) <<"." ;
           else
           {
               std::string nomElement = c.elementALaPosition(i,j)->nom();
-              std::cout<<nomElement[0];
+              std::cout<< std::setw(3) << nomElement[0];
           }
 
+
         }
+        //ligne de droite
+        std::cout<<'|';
         std::cout<<std::endl;
     }
 
+
+    // ligne du bas
+    std::cout<<' ';
+    for(int i=0; i< 3*c.nombreLignes();i++) std::cout<<'_';
+    std::cout<<std::endl;
 }
