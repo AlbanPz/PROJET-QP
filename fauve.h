e0b736ab1ba0f7a2aae2d037bd700a6418266546
Alban <101833237+AlbanPz@users.noreply.github.com>
2023-01-05 15:02:00 +0100

    Update fauve.h

--- fauve.h
+++ fauve.h
@@ -1,10 +1,13 @@
+#ifndef FAUVE_H
+#define FAUVE_H
+
 #include "mobile.h"
 
-
-class Fauve : public Mobile{
+class Fauve : public Mobile
+{
 public:
     Fauve(const std::string& nom, int x, int y, int force, int typeDeplacement);
-
-    /** Rajouté par Mdou Dian */
     bool seDeplacer(Mobile* mobile, int x, int y) override;
 };
+
+#endif // FAUVE_H
