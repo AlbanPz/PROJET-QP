3a5444253588606cb4d4d7d91440d84eeb014fc0
Alban <101833237+AlbanPz@users.noreply.github.com>
2023-01-05 16:17:58 +0100

    Update testMobile.cpp

--- testMobile.cpp
+++ testMobile.cpp
@@ -1,53 +1,9 @@
 #include "doctest.h"
 #include "mobile.h"
 
-/*
-
-TEST_CASE("[Point] Les points sont bien construits") {
-//constructeur avec x et y
-double x {31.21}, y {-32.3};
-point p1 {x,y};
-REQUIRE( p1.x() == x );
-REQUIRE( p1.y() == y );
-//constructeur par défaut
-point p2 {};
-REQUIRE( p2.x() == 0.0 );
-REQUIRE( p2.y() == 0.0 );
+TEST_CASE("Test du constructeur de Mobile") {
+    int x = 2, y = 3;
+    Mobile m {x, y};
+    REQUIRE_EQ(m.x(), x);
+    REQUIRE_EQ(m.y(), y);
 }
-<<<<<<< HEAD
-
-*/
-
-/** Cette partie ne compilait pas Cedric */
-
-/*
-=======
-*/
-
-
-/** Cette partie ne compilait pas Cedric */
-
-/*
-
->>>>>>> 9238379a0c20649091504fe304769ee693374832
-TEST_CASE("[Mobile] Le mobile est bien construit") {
- // constructeur avec x, y, force et typeDeplacement
- int x = 10, y = 15, force = 30, typeDeplacement = 10;
-
-<<<<<<< HEAD
-    Mobile a {x,y,force,typeDeplacement};
-=======
-    Mobile a {"test",x,y,force,typeDeplacement};
->>>>>>> 9238379a0c20649091504fe304769ee693374832
-
-    REQUIRE( a.x() == x );
-    REQUIRE( a.y() == y );
-    REQUIRE( a.force() == force );
-    REQUIRE( a.typeDeplacement() == typeDeplacement );
-
-}
-<<<<<<< HEAD
-*/
-//=======
-
-//>>>>>>> 9238379a0c20649091504fe304769ee693374832
