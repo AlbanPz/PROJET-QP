b5246e8ab7d71b1d2a861619dc1a81d89cbeffde
Samib68 <118477587+Samib68@users.noreply.github.com>
2023-01-05 20:13:56 +0100

    Add files via upload
    correction de la méthode chargerCarte

--- chargeurCarte.cpp
+++ chargeurCarte.cpp
@@ -1,11 +1,14 @@
 #include "chargeurCarte.h"
 #include "fstream"
 #include <vector>
-#include "element.h"
 #include "piege.h"
+#include "element.h"
+#include "fauve.h"
 #include "bloqueur.h"
+#include "joueur.h"
+#include "carte.h"
 
- void chargeurCarte::chargerCarte(const std::string nomFichier ) const
+ Carte chargeurCarte::chargerCarte(const std::string nomFichier) const
  {
      std::ifstream fic;
      fic.open(nomFichier);
@@ -13,18 +16,18 @@
      if(fic)
      {
          //on crée le tableau de fauves
-         std::vector<Fauve*> fauves;
-        //variables pour stocker la taille de la grille
-        int longeur, largeur; //variables pour stocker la taille de la grille
-        // on lit la taille de la grille
+         std::vector<Fauve*> fauves1;
+        //variables pour stocker la taille de la terrain
+        int longeur, largeur; //variables pour stocker la taille de la terrain
+        // on lit la taille de la terrain
         fic>>longeur>>largeur;
-        std::vector<std::vector<Element*>> grille;
-        //on crée la grille et on change sa taille
-        grille.resize(largeur);
+        std::vector<std::vector<Element*>> terrain;
+        //on crée la terrain et on change sa taille
+        terrain.resize(largeur);
 
-        for(int i=0;i<grille.size();i++)
+        for(unsigned int i=0;i<terrain.size();i++)
         {
-            grille[i].resize(longeur);
+            terrain[i].resize(longeur);
         }
 
         //On lit maintenant le joueur
@@ -33,32 +36,48 @@
         std::string nom;
         fic>>type>>x>>y>>puis>>type_depla;
         //on crée le joueur
-        Joueur j{type,x,y,puis,type_depla};
-  // on lit le type de l'élément si fauve -> on crée un fauve 
+        Joueur j1{type,x,y,puis,type_depla};
+  // on lit le type de l'élément si fauve -> on crée un fauve
+  int nbElem=0;
+  int i=0;
+  while(i!=nbElem)
+  {
      fic>>type;
         if(type=="fauve")
         {
             fic>>nom>>x>>y>>puis>>type_depla;
-            fauves.push_back(new Fauve{nom,x,y,puis,type_depla})
+            fauves1.push_back(new Fauve{nom,x,y,puis,type_depla});
         }
       //si piège -> on crée un piège
         else if(type=="piege")
         {
-            fic>>x>>y>>capacite;
-            grille[x][y]=new Piege{x,y,capacite};
+            fic>>nom>>x>>y>>capacite;
+            terrain[x][y]=new Piege{x,y,capacite};
         }
       //sinon on crée un bloqueur
         else
         {
             fic>>nom>>x>>y;
-            grille[x][y]=new Bloqueur{nom,x,y};
+            terrain[x][y]=new Bloqueur{nom,x,y};
         }
-// on ferme le fichier
-        fic.close();
+        i++;
      }
+     // on ferme le fichier
+     fic.close();
+     Carte Carte{j1,fauves1,terrain};
+     return Carte;
+     }
+
   //si le fichier ne s'ouvre pas, on prévient l'utilisateur
      else
      {
-         std::cout<<"impossible de lire le fichier, vérifiez le chemin d'accès";
+         std::cout<<"impossible de lire le fichier, vérifiez le chemin d'accès, carte vide chargée";
+         //création d'une carte vide
+         Joueur j{"Impossible",0,0,0,0};
+         std::vector<Fauve*> nul;
+         std::vector<std::vector<Element*>> elNul;
+         Carte cartenul{j,nul,elNul};
+         return cartenul;
      }
+
  }
