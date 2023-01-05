93b10669949b79fadb4d5c5b058438e7803fc87c
Alban <101833237+AlbanPz@users.noreply.github.com>
2023-01-05 15:04:37 +0100

    Update bloqueur.cpp

--- bloqueur.cpp
+++ bloqueur.cpp
@@ -1,11 +1,10 @@
 #include "bloqueur.h"
 
-Bloqueur::Bloqueur(const std::string& nom, int x, int y) : Element{nom, "Bloqueur", x, y}
+Bloqueur::Bloqueur(const std::string& nom, int x, int y):
+    Element{nom, "Bloqueur", x, y}
 {}
 
-/** Rajouté par Mdou Dian */
 bool Bloqueur::seDeplacer(Mobile* mobile, int x, int y)
 {
     return false;
 }
-
