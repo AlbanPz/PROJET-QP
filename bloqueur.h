5dd86a0b933cc4cf30287291fc89e960f6312b8f
Alban <101833237+AlbanPz@users.noreply.github.com>
2023-01-05 15:04:11 +0100

    Update bloqueur.h

--- bloqueur.h
+++ bloqueur.h
@@ -1,12 +1,14 @@
-#include "element.h"
+#ifndef BLOQUEUR_H
+#define BLOQUEUR_H
 
+#include "element.h"
 
 class Bloqueur : public Element
 {
 public:
     Bloqueur(const std::string& nom, int x, int y);
-    bool bloqueLePassage() const { return true; } // renvoie toujours true car elle indique que l'obstacke bloque le passage
-
-    /** Rajouté par Mdou Dian */
+    bool bloqueLePassage() const { return true; } // renvoie toujours true car il indique que l'obstacle bloque le passage
     bool seDeplacer(Mobile* mobile, int x, int y) override;
 };
+
+#endif // BLOQUEUR_H
