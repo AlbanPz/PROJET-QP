3c1bf08c151796b6eff768318cdee1b2bf97ca42
Alban <101833237+AlbanPz@users.noreply.github.com>
2023-01-05 14:58:47 +0100

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -1,15 +1,18 @@
-#include "mobile.h"
+#ifndef JOUEUR_H
+#define JOUEUR_H
 
+#include "mobile.h"
 
 class Joueur : public Mobile
 {
 public:
     Joueur(const std::string& nom, int x, int y, int force, int typeDeplacement);
-
-/** Rajouté par Mdou Dian */
     bool seDeplacer(Mobile* mobile, int x, int y) override;
     bool estVivant() const;
     void kill();
+
 private:
     bool d_vie;
 };
+
+#endif // JOUEUR_H
